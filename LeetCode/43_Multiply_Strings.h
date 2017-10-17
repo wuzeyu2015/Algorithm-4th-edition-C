1889dc1d34825801bbbc7af18f2eeda66a7bffe9
wuzeyu2015 <wu.zeyu@foxmail.com>
2017-10-17 09:21:36 +0800

    mul

---
+++ LeetCode/43_Multiply_Strings.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "stdafx.h"
+
+string multiply(string num1, string num2) {
+	string res;
+
+}
+
+void test_110() {
+	TreeNode* p = new TreeNode(0);
+	p->left = NULL;
+	p->right = NULL;
+	string s = multiply("33", "33");
+}
