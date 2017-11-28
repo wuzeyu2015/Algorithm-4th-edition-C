1c7a5abb2e79b21b68081d222e5976c655213df9
jerry <Jerry@example>
2017-11-28 18:59:50 +0800

    minpq

--- DataStruct/PQ/MinPQ.cpp
+++ DataStruct/PQ/MinPQ.cpp
@@ -1,12 +1,104 @@
+// MinPQ.cpp : Defines the entry point for the console application.
 #include "stdafx.h"
 #include "MinPQ.h"
+template<typename T>
+MinPQ<T>::MinPQ() {
 
-
-MinPQ::MinPQ()
-{
 }
 
+template<typename T>
+MinPQ<T>::MinPQ(int max) :N(0), C(max) {
 
-MinPQ::~MinPQ()
-{
 }
+
+template<typename T>
+MinPQ<T>::MinPQ(T a[], int n) : N(n), C(n) {
+ 
+}
+
+template<typename T>
+void MinPQ<T>::insert(T v) {
+ 
+}
+
+template<typename T>
+T MinPQ<T>::delMax() {
+ 
+}
+template<typename T>
+bool MinPQ<T>::isEmpty() {
+ 
+}
+
+template<typename T>
+int MinPQ<T>::size() {
+ 
+}
+
+template<typename T>
+T MinPQ<T>::max() {
+ 
+}
+
+template<typename T>
+vector<int> MinPQ<T>::heapSort() {
+ 
+}
+
+template<typename T>
+void MinPQ<T>::swim(int k) {
+ 
+	}
+}
+
+template<typename T>
+void MinPQ<T>::sink(int k) {
+ 
+}
+
+template<typename T>
+void MinPQ<T>::printTest() {
+ 
+}
+
+//////////////////////////////////////////////////////////
+//独立的堆排序接口,注意a[]从索引1开始
+template<typename T>
+void MinPQ<T>::sink(int a[], int k, int n) {			//头部新数据重排
+ 
+}
+
+template<typename T>
+void MinPQ<T>::heapSort(int a[], int n) {
+ 
+}
+
+template<typename T>
+int MinPQ<T>::HeapOpt() {
+ 
+
+	return 0;
+}
+
+class mycomparison
+{
+	bool reverse;
+public:
+	mycomparison(const bool& revparam = false)
+	{
+		reverse = revparam;
+	}
+	bool operator() (const int& lhs, const int&rhs) const
+	{
+		if (reverse) return (lhs>rhs);
+		else return (lhs<rhs);
+	}
+};
+
+template<typename T>
+int MinPQ<T>::pq_Container()
+{
+ 
+}
+
+template class MinPQ<int>;
--- DataStruct/PQ/MinPQ.h
+++ DataStruct/PQ/MinPQ.h
@@ -1,8 +1,48 @@
-#pragma once
+#ifndef MINPQ_H
+#define MINPQ_H
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <ctime>
+#include <cmath>
+#include <cassert>
+#include <vector>
+#include <queue>
+#include<functional>
+using namespace std;
+
+template<typename T>
 class MinPQ
 {
 public:
-	MinPQ();
-	~MinPQ();
+public:
+	MinPQ();							//默认构造空队列
+	MinPQ(int n);						//用n构造
+	MinPQ(T a[], int n);				//用数组构造
+
+	void insert(T v);					//末尾插入 
+	T	 delMax();						//头部删除 
+	bool isEmpty();						//判空
+	int  size();						//队列大小
+	T	 min();							//访问最小元素
+
+
+private:
+	void swim(int k);					//尾部新数据重排
+	void sink(int k);					//头部新数据重排
+	void printTest();					//打印堆数据
+
+	T* pq;								//内置数组
+	int N;								//当前数据量
+	int C;								//数据容量
+
+public:
+	static void sink(int a[], int k, int n);
+	static void heapSort(int a[], int n);
+	static int MinPQ<T>::pq_Container();
+	static int MinPQ<T>::HeapOpt();
 };
 
+#endif //MINPQ_H
