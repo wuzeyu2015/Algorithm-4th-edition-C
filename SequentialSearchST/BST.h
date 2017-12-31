ba4daecb1277ebfcd53b8f915aa94b1e39941246
jerry <Jerry@example>
2017-12-31 12:58:10 +0800

    //

--- SequentialSearchST/BST.h
+++ SequentialSearchST/BST.h
@@ -47,17 +47,20 @@ public:
 	void deleteMax();
 	int size(Key lo, Key hi); //number of keys in[lo..hi]
 	vector<Key>* keys(Key lo, Key hi); //keys in[lo..hi], in sorted order
+
 private:
 	int rank(Key key);
+	int size(TreeNode* root);
 	Value get(TreeNode* root, Key key);
 	TreeNode*& put(TreeNode*& pnode, Key key, Value val);
 	void keys(TreeNode* proot, vector<Key>* pkeyarr);
+
+	TreeNode* floor(TreeNode* pNode, Key key);
 public:
 	//test entrance
 	static void main(int minLen);
 
 private:
-	int N;
 	TreeNode* proot; // root of BST
 
 };
